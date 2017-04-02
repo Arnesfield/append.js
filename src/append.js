275954050e58b761eb606998260ef0a07d9a3ff2
Jefferson Rylee <Arnesfield@users.noreply.github.com>
2017-04-02 17:00:52 +0800

    Create append.js

---
+++ src/append.js
@@ -0,0 +1,58 @@
+// use: append.these('my-');
+
+var append = {
+  these : function(s) {
+    // split prefixes
+    var args = s.trim().split(' ');
+    if (!args.length) return;
+    
+    // create regex
+    var regex = args.join('|');
+    regex = '(^(' + regex + ') )|( (' + regex + ') )';
+    
+    // for each selector
+    $(':regex(class,' + regex + ')').each(function() {
+      var e = $(this), retain = 0, remove = '';
+      
+      // while this has class from args
+      while ((function() {
+        var b = 0;
+        args.forEach(function(f) {
+          if (!b && e.hasClass(f)) b = 1;
+        });
+        return b;
+      })()) {
+        function get(start) {
+          var pos = e.attr('class').length;
+          args.forEach(function(f) {
+            var i = e.attr('class').indexOf(f);
+            if (i <= pos && i >= start) {
+              pos = i;
+              arg = f;
+            }
+          });
+          return {
+            arg: arg, pos: pos
+          };
+        }
+        
+        var arg1 = get(0);
+        e.removeClass(arg1.arg);
+        var arg2 = get(arg1.pos);
+        
+        retain = (retain === 0) ? e.attr('class').substr(0,arg1.pos).trim() : retain;
+        
+        var toAddStr = e.attr('class').substring(arg1.pos, arg2.pos).trim().split(' ');
+        if (toAddStr != '') {
+          toAddStr.forEach(function(f) {
+            var pos = /[_a-zA-Z]/.exec(f).index;
+            e.addClass(f.substr(0, pos) + arg1.arg + f.substr(pos));
+            remove += f + ' ';
+          });
+        }
+      }
+      
+      e.removeClass(remove).addClass(retain);
+    });
+  }
+};
